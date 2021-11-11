0d75f3d7c05bbc99a531cc54b660fd9318a9d491
Rameenh <Rameenh@live.com>
2021-11-10 18:15:45 -0600

    Got all sorts working within if statements for sort mode

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from pathlib import Path
+from datetime import datetime
 
 
 def main():
     global inventory
     inventory = open('inventory.txt', 'r')  # This is to create the inventory file, only if it doesn't already exist
-    send_sorted()
+    send_sorted("date")
 
 
-def send_sorted():
+def send_sorted(mode):
     inventory_list = []
     row = inventory.readlines()
     for line in row:
         inventory_list.append(line.split())
 
-    sorted_name = sorted(inventory_list, key=lambda x: x[0])
+    if mode == 'name':
+        sorted_inventory = sorted(inventory_list, key=lambda x: x[0])
+    elif mode == 'quantity':
+        sorted_inventory = sorted(inventory_list, key=lambda x: int(x[1]))
+    elif mode == 'date':
+        sorted_inventory = sorted(inventory_list, key=lambda x: datetime.strptime(x[2], '%m-%d-%Y'))
+    else:
+        print("Invalid mode specified for sort function")
 
-    print(sorted_name)
+    print(sorted_inventory)
 
 main()
