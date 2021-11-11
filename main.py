b5ac0d397052c098aa26368efb0f90b9b0f695fe
Rameenh <Rameenh@live.com>
2021-11-10 23:30:34 -0600

    remove function working

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from pathlib import Path
 from datetime import datetime
 
 
 def main():
     global inventory
     inventory = open('inventory.txt', 'r')  # This is to create the inventory file, only if it doesn't already exist
-    send_sorted("date")
-
-
-def send_sorted(mode):
+    global inventory_list
     inventory_list = []
     row = inventory.readlines()
     for line in row:
         inventory_list.append(line.split())
+    #send_sorted("date")
+    #remove("Pear")
 
+
+def send_sorted(mode):
     if mode == 'name':
         sorted_inventory = sorted(inventory_list, key=lambda x: x[0])
     elif mode == 'quantity':
@@ -23,6 +23,15 @@ def send_sorted(mode):
     else:
         print("Invalid mode specified for sort function")
 
-    print(sorted_inventory)
+    #print(sorted_inventory)
+
+
+def remove(name):
+    #print (inventory_list)
+    print('\n')
+    x = [x for x in inventory_list if name in x][0]
+    inventory_list.pop(inventory_list.index(x))
+    #print(inventory_list)
+
 
 main()
