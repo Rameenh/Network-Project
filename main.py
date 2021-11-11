ecea98d0acf47adc2a164a6eaf9efd9584deacb4
Rameenh <Rameenh@live.com>
2021-11-10 18:00:35 -0600

    Got import list and split to 2D matrix working

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from pathlib import Path
 
 
 def main():
-    inventory = open('inventory.txt', 'a')  #This is to create the inventory file, only if it doesn't already exist
+    global inventory
+    inventory = open('inventory.txt', 'r')  # This is to create the inventory file, only if it doesn't already exist
+    send_sorted()
 
-def
 
+def send_sorted():
+    inventory_list = []
+    row = inventory.readlines()
+    for line in row:
+        inventory_list.append(line.split())
 
+    for x in range(len(inventory_list)):
+        print(inventory_list[x])
+
+main()
