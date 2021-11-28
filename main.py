ad830bc9961eb946f38170e8e8aa61e25e5f1450
Rameenh <Rameenh@live.com>
2021-11-28 15:03:24 -0600

    got change file content in remove working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from socket import *
 
 
 def main():
@@ -10,17 +11,55 @@ def main():
     for line in row:
         inventory_list.append(line.split())
     # send_sorted("date")
-    remove("Pear")
-    #update_quantity("Xbox", 18)
+    # remove("Pear")
+    # update_quantity("Xbox", 18)
+
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    webPort = 65432
+    serverSocket.bind(('127.0.0.1', webPort))
+
+    serverSocket.listen(1)
+
+    connectionSocket, addr = serverSocket.accept()
+
+    while True:
+        print("connected")
+
+        try:
+            message = connectionSocket.recv(1024).decode()
+            remote_input = message.split()
+            print(remote_input)
+            if (remote_input[0] == '1'):
+                mode = remote_input[1]
+                tmp = send_sorted(mode)
+
+                tmp = str(tmp)
+
+                connectionSocket.sendall(tmp.encode())
+
+            elif (remote_input[0] == '2'):
+                item_to_rem = remote_input[1]
+                remove(item_to_rem)
+            elif (remote_input[0] == '3'):
+                item_to_upd = remote_input[1]
+                new_num = remote_input[2]
+                update_quantity(item_to_upd, new_num)
+            else:
+                print('invalid remote input')
+
+        except IOError:
+            print("IOError")
+            connectionSocket.close()
+            serverSocket.close()
 
 
 def send_sorted(mode):
     if mode == 'name':
-        sorted_inventory = sorted(inventory_list, key=lambda x: x[0])
+        return sorted(inventory_list, key=lambda x: x[0])
     elif mode == 'quantity':
-        sorted_inventory = sorted(inventory_list, key=lambda x: int(x[1]))
+        return sorted(inventory_list, key=lambda x: int(x[1]))
     elif mode == 'date':
-        sorted_inventory = sorted(inventory_list, key=lambda x: datetime.strptime(x[2], '%m-%d-%Y'))
+        return sorted(inventory_list, key=lambda x: datetime.strptime(x[2], '%m-%d-%Y'))
     else:
         print("Invalid mode specified for sort function")
 
